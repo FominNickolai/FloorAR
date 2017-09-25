f6833a5ab4b6cb0e744ddb5ffdb5a0b9e94a499a
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-09-25 10:41:21 +0300

    Created laveNode and added to the plane surface. Updates size and position if needed.

--- FloorAR/ViewController.swift
+++ FloorAR/ViewController.swift
@@ -20,10 +20,82 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.configuration.planeDetection = .horizontal
         self.sceneView.session.run(configuration, options: [])
+        self.sceneView.delegate = self
         
+    }
+    
+    //Create Lava node
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.eulerAngles = SCNVector3(90.degreesToRadians, 0, 0)
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        
+        return lavaNode
     }
 
 
 }
+
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+        
+        print("new flat surface detected, new ARPlaneAnchor added")
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        node.addChildNode(lavaNode)
+        
+        
+        print("updated ARPlaneAnchor")
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let _ = anchor as? ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        
+        print("didRemove ARPlaneAnchor")
+        
+    }
+    
+}
+
+
+extension Int {
+    var degreesToRadians: Double {
+        return Double(self) * .pi / 180
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
