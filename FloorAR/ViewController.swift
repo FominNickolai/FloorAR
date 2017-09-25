6ad76abae61a1e4fecb7df43e51db20f12317b56
Fomin Nickolai <giraffe.in.ua@gmail.com>
2017-09-25 09:37:10 +0300

    Added ARSCNView. Run WorldTracking.

--- FloorAR/ViewController.swift
+++ FloorAR/ViewController.swift
@@ -7,17 +7,21 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration, options: [])
+        
     }
 
 
